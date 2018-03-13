80ed45c1e665f22319050a969db2631144092ab8
vagrant <vagrant@homestead>
2018-03-13 08:18:08 +0000

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/','PagesController@root')->name('root');
 Auth::routes();
 
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
